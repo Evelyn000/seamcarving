b6ca0371a98fc1c76f13d8dc29bca35e6bd56702
GoodMorning <ma16_helen@pku.edu.cn>
2018-06-16 16:48:33 +0800

    test

--- energy_class_new.py
+++ energy_class_new.py
@@ -11,9 +11,9 @@ import matplotlib.image as mpimg # mpimg 用于读取图片
 import numpy as np
 import cv2
 #import Image
-img = mpimg.imread('coast.jpg') # 读取和代码处于同一目录下的 coast.jpg
+#img = mpimg.imread('coast.jpg') # 读取和代码处于同一目录下的 coast.jpg
 # 此时 img 就已经是一个 np.array 了，可以对它进行任意处理
-print(img.shape) #(400, 600, 3)
+# print(img.shape) #(400, 600, 3)
 
 plt.subplot(231)
 plt.imshow(img) # 显示图片
